3ec267fc1c57ad3fa785b1a1ce68e1593e760128
killerontheradio <129413793+killerontheradio@users.noreply.github.com>
2026-08-28 20:08:34 +0530

    Add project files.

---
+++ build/CMakeFiles/tracerola.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tracerola.dir/tracerola.cpp.obj"
+  "CMakeFiles/tracerola.dir/tracerola.cpp.obj.d"
+  "tracerola.exe"
+  "tracerola.exe.manifest"
+  "tracerola.lib"
+  "tracerola.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tracerola.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ color.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void put_pixel(std::ostream& out, const color& pixel_color)
+{
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	int rbyte = static_cast<int>(255.999 * r);
+	int gbyte = static_cast<int>(255.999 * g);
+	int bbyte = static_cast<int>(255.999 * b);
+
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+
+
+#endif // !COLOR_H
+
